e90116fac1d70e4604354f18dddfb28581f62894
PauloDavidWebSolutions <141250325+PauloDavidWebSolutions@users.noreply.github.com>
2024-08-28 16:37:34 +0100

    emails tra

--- override/classes/order/OrderHistory.php
+++ override/classes/order/OrderHistory.php
@@ -782,21 +782,21 @@ class OrderHistoryCore extends ObjectModel
                 <tr>
                     <td>
                         <p>
-                        '.$this->trans('As you selected credit card as payment method for this order, you will find below a link that will allow you to make the payment through our financial partner platform. This completely secure link will be active only for 48 hours and will automatically update the status of your order once validated.', [], 'Shop.Theme.Email').'
+                        '.$this->trans('As you selected credit card as payment method for this order, you will find below a link that will allow you to make the payment through our financial partner platform. This completely secure link will be active only for 48 hours and will automatically update the status of your order once validated.', [], 'Shop.Theme.Global').'
                         </p>
                     </td>
                 </tr>
                 <tr>
                     <td>
                         <a href="http://webtools.euromuscleparts.com/customTools/worldline/validate?order_reference='.$reference.'" style="background-color: #0273eb; color: white; padding: .5rem 1rem; border: none; cursor: pointer;border-radius: .25rem;">
-                            '.$this->trans('Link', [], 'Shop.Theme.Email').'
+                            '.$this->trans('Link', [], 'Shop.Theme.Global').'
                         </a>
                     </td>
                 </tr>
             </table>';
         }else{
             return '<p>
-                '.$this->trans('As you selected bank transfer as payment method for this order, we kindly ask you to make this transfer to our Portuguese account (Millennium BCP Bank) via the bank details provided when creating your dealer account. Any email requesting payment to another account should be considered fraudulent. Do not hesitate to contact us for more information.', [], 'Shop.Theme.Email').'
+                '.$this->trans('As you selected bank transfer as payment method for this order, we kindly ask you to make this transfer to our Portuguese account (Millennium BCP Bank) via the bank details provided when creating your dealer account. Any email requesting payment to another account should be considered fraudulent. Do not hesitate to contact us for more information.', [], 'Shop.Theme.Global').'
             </p>';
         }
 
