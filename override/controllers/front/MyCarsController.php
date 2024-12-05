176b459fd9696fd81c4d918b30e9bfa0d1085e74
PauloDavidWebSolutions <141250325+PauloDavidWebSolutions@users.noreply.github.com>
2024-12-05 17:50:26 +0000

    my cars controller

--- override/controllers/front/MyCarsController.php
+++ override/controllers/front/MyCarsController.php
@@ -13,9 +13,9 @@ class MyCarsControllerCore extends FrontController
         if(Tools::getValue('delete') == true){
             
 
-            $my_cars = Db::getInstance()->executes("DELETE FROM "._DB_PREFIX_."ASM_ukoo_customer WHERE id = " . Tools::getValue('id'));
+            $my_cars = Db::getInstance()->executes("DELETE FROM "._DB_PREFIX_."asm_ukoo_customer WHERE id = " . Tools::getValue('id'));
             
-            $my_cars = Db::getInstance()->executes("Select * FROM "._DB_PREFIX_."ASM_ukoo_customer WHERE email = '" . $this->context->customer->email . "'");
+            $my_cars = Db::getInstance()->executes("SELECT * FROM "._DB_PREFIX_."asm_ukoo_customer WHERE email = '" . $this->context->customer->email . "'");
             $this->context->smarty->assign('myCars', $my_cars);
             $this->setTemplate(_PS_THEME_DIR_.'my-cars.tpl');
             
@@ -25,8 +25,8 @@ class MyCarsControllerCore extends FrontController
             
         }elseif(Tools::getValue('unsubscribe') == 1){
             
-            $my_cars = Db::getInstance()->executes("UPDATE "._DB_PREFIX_."ASM_ukoo_customer SET newsletter=0 WHERE email='" . Tools::getValue('email') . "'");
-            $my_cars = Db::getInstance()->executes("Select * FROM "._DB_PREFIX_."ASM_ukoo_customer WHERE email = '" . $this->context->customer->email . "'");
+            $my_cars = Db::getInstance()->executes("UPDATE "._DB_PREFIX_."asm_ukoo_customer SET newsletter=0 WHERE email='" . Tools::getValue('email') . "'");
+            $my_cars = Db::getInstance()->executes("SELECT * FROM "._DB_PREFIX_."asm_ukoo_customer WHERE email = '" . $this->context->customer->email . "'");
             $this->context->smarty->assign('email', Tools::getValue('email'));
             $this->context->smarty->assign('unsubscribeMessage', 1);
             $this->context->smarty->assign('myCars', $my_cars);
@@ -34,8 +34,8 @@ class MyCarsControllerCore extends FrontController
 
         }elseif(Tools::getValue('getEmails') == 1){
 
-            $car = Db::getInstance()->executes("Select * FROM "._DB_PREFIX_."ASM_ukoo_customer WHERE id_brand = " . Tools::getValue('id_brand') . " AND id_model=" . Tools::getValue('id_model') . " AND id_type=" . Tools::getValue('id_type') . " AND id_version=" . Tools::getValue('id_version') . ' LIMIT 1');
-            $emails = Db::getInstance()->executes("Select email FROM "._DB_PREFIX_."ASM_ukoo_customer WHERE id_brand = " . Tools::getValue('id_brand') . " AND id_model=" . Tools::getValue('id_model') . " AND id_type=" . Tools::getValue('id_type') . " AND id_version=" . Tools::getValue('id_version'));
+            $car = Db::getInstance()->executes("SELECT * FROM "._DB_PREFIX_."asm_ukoo_customer WHERE id_brand = " . Tools::getValue('id_brand') . " AND id_model=" . Tools::getValue('id_model') . " AND id_type=" . Tools::getValue('id_type') . " AND id_version=" . Tools::getValue('id_version') . ' LIMIT 1');
+            $emails = Db::getInstance()->executes("SELECT email FROM "._DB_PREFIX_."asm_ukoo_customer WHERE id_brand = " . Tools::getValue('id_brand') . " AND id_model=" . Tools::getValue('id_model') . " AND id_type=" . Tools::getValue('id_type') . " AND id_version=" . Tools::getValue('id_version'));
 
             $car_string = $car[0]['brand'] . ' | ' . $car[0]['model'] . ' | ' . $car[0]['version'] . ' | ' . $car[0]['type'];
             $id_brand = $car[0]['id_brand'];
@@ -49,7 +49,7 @@ class MyCarsControllerCore extends FrontController
             return $array;
             
         }else{
-            $my_cars = Db::getInstance()->executes("Select * FROM "._DB_PREFIX_."ASM_ukoo_customer WHERE email = '" . $this->context->customer->email . "'");
+            $my_cars = Db::getInstance()->executes("SELECT * FROM "._DB_PREFIX_."asm_ukoo_customer WHERE email = '" . $this->context->customer->email . "'");
             $this->context->smarty->assign('myCars', $my_cars);
             $this->setTemplate('customer/my-cars');
             
