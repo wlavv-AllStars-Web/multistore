abae752a43fc7e56a0a4bf8fd67df11407694cf8
PauloDavidWebSolutions <141250325+PauloDavidWebSolutions@users.noreply.github.com>
2025-03-12 16:32:49 +0000

    ql

--- override/controllers/front/ContactController.php
+++ override/controllers/front/ContactController.php
@@ -216,15 +216,15 @@ class ContactControllerCore extends FrontController
                             // exit;
         
                             if (!empty($contact->email) || !empty(Tools::getValue('from'))) {
-                                // if (!Mail::Send(2, 'contact', Mail::l('Message from contact form').' [no_sync]',
-                                //     $var_list, 'info@euromuscleparts.com', $contact->name, null, null,
-                                //             $file_attachment, null,    _PS_MAIL_DIR_, false, null, null, $from)) {
-                                //     $this->errors[] = Tools::displayError('An error occurred while sending the message.');
-                                // }
+                                if (!Mail::Send(2, 'contact', Mail::l('Message from contact form').' [no_sync]',
+                                    $var_list, 'info@euromuscleparts.com', $contact->name, null, null,
+                                            $file_attachment, null,    _PS_MAIL_DIR_, false, null, null, $from)) {
+                                    $this->errors[] = Tools::displayError('An error occurred while sending the message.');
+                                }
                                 
-                                Mail::Send($this->context->language->id, 'contact_form', Mail::l('Message from contact form').' [no_sync]',
-                                    $var_list, 'info@euromuscleparts.com', $var_list['{firstname}'] . ' ' . $var_list['{lastname}'], null, null,
-                                            $file_attachment, null,    _PS_MAIL_DIR_, false, null, null, $from);
+                                // Mail::Send($this->context->language->id, 'contact_form', Mail::l('Message from contact form').' [no_sync]',
+                                //     $var_list, 'info@euromuscleparts.com', $var_list['{firstname}'] . ' ' . $var_list['{lastname}'], null, null,
+                                //             $file_attachment, null,    _PS_MAIL_DIR_, false, null, null, $from);
                             }
                         }
         
