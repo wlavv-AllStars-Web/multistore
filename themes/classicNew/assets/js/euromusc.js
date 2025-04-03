d456d1a84e18efb565bacdb85ff1d29f69f8b795
PauloDavidWebSolutions <141250325+PauloDavidWebSolutions@users.noreply.github.com>
2025-04-03 10:34:36 +0100

    js

--- themes/classicNew/assets/js/euromusc.js
+++ themes/classicNew/assets/js/euromusc.js
@@ -25,3 +25,62 @@ function toggleSearchbar() {
     }
 
 }
+
+
+// menu brands
+
+document.addEventListener('DOMContentLoaded', function() {
+// dropdown brands
+const dropdownLi = document.querySelector(".mainmenuDesktop .dropdown.brands-drop")
+const dropdownBrands = document.querySelector('a.dropdown-toggle-brands');
+// const dropdownBrandsCaret = document.querySelector('li.dropdown i');
+const dropdownContent = document.querySelector('ul.dropdown-content');
+
+
+dropdownBrands.addEventListener('click', (e) => {
+  e.stopPropagation();
+  toggleDropdown();
+});
+
+if(window.screen.width > 767){
+  dropdownLi.addEventListener('mouseover', (e) => {
+    e.stopPropagation();
+    toggleDropdown();
+
+    document.querySelectorAll(".products-sort-order .dropdown-menu").forEach((drop) => {
+      drop.style.display = "none"
+    })
+  });
+  dropdownLi.addEventListener('mouseout', (e) => {
+    e.stopPropagation();
+    closeDropdown();
+    document.querySelectorAll(".products-sort-order .dropdown-menu").forEach((drop) => {
+      drop.style.removeProperty("display");
+    })
+  });
+
+}
+
+// Add event listener to close dropdown on clicks outside
+document.addEventListener('click', (e) => {
+  const isClickInsideDropdown = dropdownBrands.contains(e.target) || dropdownContent.contains(e.target);
+
+  if (!isClickInsideDropdown) {
+    closeDropdown();
+  }
+});
+
+function toggleDropdown() {
+  if (!dropdownContent.style.display || dropdownContent.style.display === "none") {
+    dropdownContent.style.display = "flex";
+    dropdownContent.style.flexWrap = "wrap";
+  } else {
+    closeDropdown();
+  }
+}
+
+function closeDropdown() {
+  dropdownContent.style.display = "none";
+}
+
+});
