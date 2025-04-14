ae7e1108358e0a40153ac24fb48ae1c7b7ff7b49
PauloDavidWebSolutions <pauloallstarsweb@gmail.com>
2025-04-14 09:17:19 +0100

    urls

--- override/controllers/front/IndexController.php
+++ override/controllers/front/IndexController.php
@@ -229,7 +229,7 @@ class IndexController extends IndexControllerCore
         $store = $id_shop;
 
         $urlBrands = 'https://webtools.'.$_SERVER['SERVER_NAME'].'/api/get/brands/'.$store.'/'. $key;
-        echo 'urlbrands' . $urlBrands;
+        echo 'urlbrands: ' . $urlBrands;
         // exit;
 
         // $urlModels = 'https://webtools.'.$_SERVER['SERVER_NAME'].'/api/get/brand/'.$brand.'/2/'.$key;
