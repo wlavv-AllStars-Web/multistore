ebb9499867880c5fc40958be3890a484f8227845
PauloDavidWebSolutions <pauloallstarsweb@gmail.com>
2025-04-30 10:23:38 +0100

    js

--- themes/classicNew/assets/js/euromusc.js
+++ themes/classicNew/assets/js/euromusc.js
@@ -159,10 +159,24 @@ var Swipes2 = new Swiper('.products-mobile', {
 });
 
 var swiper = new Swiper(".swiperCars", {
-  slidesPerView: 4,
+  slidesPerView: 1,
   spaceBetween: 30,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 5,
+      spaceBetween: 50,
+    },
   },
 });
