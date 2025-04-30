38c171f4c8d462f165902d389581f73f24a1601c
PauloDavidWebSolutions <pauloallstarsweb@gmail.com>
2025-04-30 10:53:35 +0100

    navigations

--- themes/classicNew/assets/js/euromusc.js
+++ themes/classicNew/assets/js/euromusc.js
@@ -165,6 +165,10 @@ var swiper = new Swiper(".swiperCars", {
     el: ".swiper-pagination",
     clickable: true,
   },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
   breakpoints: {
     640: {
       slidesPerView: 1,
