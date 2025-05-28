206e01125c04c21104c1d2d2b4402a05c3410739
PauloDavidWebSolutions <pauloallstarsweb@gmail.com>
2025-05-28 17:21:26 +0100

    barcode-white

--- modules/asgroup/views/js/orders.js
+++ modules/asgroup/views/js/orders.js
@@ -359,7 +359,7 @@ document.addEventListener("DOMContentLoaded", () => {
             btn_detailsEAN.style.display = 'flex'
             btn_detailsEAN.style.padding = '0 .5rem'
             btn_detailsEAN.innerHTML = ''
-            btn_detailsEAN.innerHTML = '<i class="material-icons" style="transform: rotate(90deg);">dehaze</i>'
+            btn_detailsEAN.innerHTML = '<i class="barcode-white"> </i>'
         }
     }
 
