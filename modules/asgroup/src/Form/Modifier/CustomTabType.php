746681f7668d065bc09d2397695c197c71a3e66c
PauloDavidWebSolutions <pauloallstarsweb@gmail.com>
2025-06-05 16:26:54 +0100

    custom type tab

--- modules/asgroup/src/Form/Modifier/CustomTabType.php
+++ modules/asgroup/src/Form/Modifier/CustomTabType.php
@@ -17,8 +17,15 @@ namespace PrestaShop\Module\AsGroup\Form\Modifier;
 if (!defined('_PS_VERSION_')) {
     exit;
 }
+
+use PrestaShopBundle\Form\Admin\Type\TranslateType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\BaseType;
+use Symfony\Component\Form\Extension\Core\Type as FormType;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
 class CustomTabType extends BaseType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
@@ -30,5 +37,13 @@ class CustomTabType extends BaseType
                 'form_theme' => '@Modules/asgroup/views/templates/admin/custom_html_block.html.twig',
             ]
         );
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        parent::configureOptions($resolver);
+        $resolver->setDefaults([
+            'form_theme' => '@Modules/asgroup/views/templates/admin/custom_html_block.html.twig',
+        ]);
     }
 }
